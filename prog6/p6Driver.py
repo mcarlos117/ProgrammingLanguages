f9cb70ce3580b253a05d4e33d4f4d6327a566311
mcarlos117 <miguelcarlos117@yahoo.com>
2017-07-14 10:45:43 -0500

    Create p6Driver.py

---
+++ prog6/p6Driver.py
@@ -0,0 +1,23 @@
+# Program 6 Driver
+
+# Imports the function 'printLines' from file 'p5.py'
+from p6 import readLines
+
+
+# Executes the function printLines.
+readLines()
+#while True:
+#    try:
+#        inputLines = readLines()
+#        print(readLines())
+        #lineSplits = inputLines.split()
+        #if lineSplits[0] == "CUSTOMER":
+            #count = 0
+            #printCust
+
+        #if lineSplits [0] == "ADDREND":
+            #count += 1
+            #printLines()
+#    except (EOFError):
+#        break
+
